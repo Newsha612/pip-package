02163b7e81ae3ba2f02b3b990b3b07560749ff8c
Newsha612 <32583732+Newsha612@users.noreply.github.com>
2019-05-07 12:18:30 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 11:58:52 2019
+
+@author: newsh
+"""
+
+from setuptools import setup
+
+setup(
+      name='PathPlanning',
+      version = '0.1',
+      scripts = ['PathPlanning']
+      )
