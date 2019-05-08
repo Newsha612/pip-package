cdf7b34918547e0da620611a30bdd8998ff695b9
Newsha612 <32583732+Newsha612@users.noreply.github.com>
2019-05-08 13:05:20 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 13:04:20 2019
+
+@author: newsh
+"""
+
+from setuptools import setup
+
+setup(name='gym_Path',
+      version='0.0.1',
+      install_requires=['gym>=0.2.3',
+                        'hfo_py>=0.2']
+)
